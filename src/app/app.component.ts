9e2bf6ac10bded631ef57f87e816f1d32f5dc11e
Albert <krm9346@autuni.ac.nz>
2024-12-30 11:04:29 +0700

    update button toggle and checkbox

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { AutocompleteSimpleExample } from './autocomplete/autoComplete.component';
 import { BadgeOverviewExample } from './Badge/badge-overview-example.component';
 import { BottomSheetOverviewExample } from './bottomSheet/bottom-sheet-overview-example.component';
+import { ButtonToggleModeExample } from './button toggle/button-toggle-mode-example.component';
+import { CheckboxOverviewExample } from './checkbox/checkbox-overview-example.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AutocompleteSimpleExample, BadgeOverviewExample, BottomSheetOverviewExample],
+  imports: [RouterOutlet, AutocompleteSimpleExample, BadgeOverviewExample, 
+    BottomSheetOverviewExample, ButtonToggleModeExample, CheckboxOverviewExample,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

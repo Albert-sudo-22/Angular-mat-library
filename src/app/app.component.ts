c42967b3a6dfe19deb57d2c616fe318cc18e9f71
Albert <krm9346@autuni.ac.nz>
2024-12-30 10:28:31 +0700

    update auto complete, badge, bottom sheet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AutocompleteSimpleExample } from './autocomplete/autoComplete.component';
+import { BadgeOverviewExample } from './Badge/badge-overview-example.component';
+import { BottomSheetOverviewExample } from './bottomSheet/bottom-sheet-overview-example.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AutocompleteSimpleExample, BadgeOverviewExample, BottomSheetOverviewExample],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/autocomplete/autoComplete.component.ts
@@ -0,0 +1,42 @@
+import {Component} from '@angular/core';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { Observable, startWith, map } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+
+/**
+ * @title Simple autocomplete
+ */
+@Component({
+  selector: 'autocomplete-simple-example',
+  templateUrl: './autoComplete.component.html',
+  styleUrl: './autoComplete.component.css',
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    AsyncPipe,
+  ],
+})
+export class AutocompleteSimpleExample {
+  myControl = new FormControl('');
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions!: Observable<string[]>;
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '')),
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
+}
